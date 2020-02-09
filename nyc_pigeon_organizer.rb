f610fde599f73dc6668b165498b94420d7a0d630
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-09 19:49:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,19 +1,17 @@
 def nyc_pigeon_organizer(data)
-  hash = {}
+def nyc_pigeon_organizer(data)
+  pigeon_list = {}
 
-  data.each do |color_gender_lives, value|
-    value.each do |stats, all_names|
-      all_names.each do |name|
-        if pigeon_list[name] == nil
-          pigeon_list[name] = {}
-        end
-        if pigeon_list[name][color_gender_lives] == nil
-          pigeon_list[name][color_gender_lives] = []
-        end
-        pigeon_list[name][color_gender_lives].push(stats.to_s)
+  data.each do |attribute_name, attributes|
+    attributes.each do |attribute_value, pigeon_names|
+      pigeon_names.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][attribute_name] ||= []
+        pigeon_list[name][attribute_name].push(attribute_value.to_s)
       end
     end 
   end
+
   pigeon_list
-  end
+end
 end
