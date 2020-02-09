23424a398f2de98eb957a45803f9e79b57c862c6
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-09 19:18:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,6 @@
 def nyc_pigeon_organizer(data)
   hash = {}
-  
-  pigeon_list = {}
+
   data.each do |color_gender_lives, value|
     value.each do |stats, all_names|
       all_names.each do |name|
@@ -16,5 +15,5 @@ def nyc_pigeon_organizer(data)
     end 
   end
   pigeon_list
-end
+  end
 end
