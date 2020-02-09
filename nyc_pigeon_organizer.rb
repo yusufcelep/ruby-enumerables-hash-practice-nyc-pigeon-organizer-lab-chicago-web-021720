94e851b153de974d2cc548c66a183bc66e951384
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-09 19:09:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,11 @@
 def nyc_pigeon_organizer(data)
   hash = {
-    "Theo": "value_one",
-    "Peter Jr.": "value_two",
-    "Lucky": "value_three",
-    "Ms. K": "value_four",
-    "Queenie": "value_five",
-    "Andrew": "value_six",
-    "Alex": "value_seven"
+    "Theo": {},
+    "Peter Jr.": {},
+    "Lucky": {},
+    "Ms. K": {},
+    "Queenie": {},
+    "Andrew": {},
+    "Alex": {}
   }
 end
