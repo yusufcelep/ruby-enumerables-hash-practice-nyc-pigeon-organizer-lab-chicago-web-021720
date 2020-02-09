f8e8482d164a60cba45bc773addb7a51c7d99f17
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-09 20:01:08 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |attribute_name, attributes|
+    attributes.each do |attribute_value, pigeon_names|
+      pigeon_names.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][attribute_name] ||= []
+        pigeon_list[name][attribute_name].push(attribute_value.to_s)
+      end
+    end 
+  end
+  pigeon_list
 end
